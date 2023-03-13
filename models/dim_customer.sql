043ba2e53166c78c16c05be73aa3af32888ed70c
aparna.menon@diggibyte.com <aparna.menon@diggibyte.com>
2023-03-13 09:36:47 +0000

    Staging added

--- models/dim_customer.sql
+++ models/dim_customer.sql
@@ -1,46 +1,14 @@
-with
-    customers as (
-
-        select id as customer_id, first_name, last_name
-        from default.jaffle_shop_customers
-
-    ),
-
-    orders as (
-
-        select id as order_id, user_id as customer_id, order_date, status
-
-        from default.jaffle_shop_orders
-
-    ),
-
-    customer_orders as (
-
-        select
-            customer_id,
-
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
-
-        from orders
-
-        group by 1
-
-    ),
-
-
-    final as (
-
-        select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-        from customer left join customer_orders using (customer_id)
-    )
-select *
-from final
+SELECT
+    customers.id AS customer_id,
+    customers.first_name,
+    customers.last_name,
+    MIN(orders.order_date) AS first_order_date,
+    MAX(orders.order_date) AS most_recent_order_date,
+    COALESCE(COUNT(orders.id), 0) AS number_of_orders
+FROM
+    default.jaffle_shop_customers AS customers
+    LEFT JOIN default.jaffle_shop_orders AS orders ON customers.id = orders.user_id
+GROUP BY
+    customers.id,
+    customers.first_name,
+    customers.last_name;
