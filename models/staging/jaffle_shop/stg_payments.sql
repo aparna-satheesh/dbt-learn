6dea72dac1f092f9e3724a63bfaf6654484c61c7
aparna.menon@diggibyte.com <aparna.menon@diggibyte.com>
2023-03-13 12:45:08 +0000

    Refactored fact_orders

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -1,1 +1,9 @@
-select * from stripe_payments
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at
+
+from stripe_payments 
